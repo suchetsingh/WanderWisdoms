8be24a9307529deeb056f91e7c475adddc334798
suchetsingh <ssingh21be20@gmail.com>
2024-03-03 17:30:23 +0530

    Created Update functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+import { test, updateUser } from "../controllers/user.controller.js";
+import { verifiyToken } from "../utils/verifiedUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+router.put("/update/:userId",verifiyToken,updateUser);
 
 export default router;
