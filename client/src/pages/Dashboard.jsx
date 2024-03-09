1656a7e07ba3abbde66c812bfe3d8736776e9163
suchetsingh <ssingh21be20@gmail.com>
2024-03-09 17:33:49 +0530

    Added DashPost Section

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSideBar from "../components/DashSideBar";
 import DashProfile from "../components/DashProfile";
+import DashPost from "../components/DashPost";
 
 export default function Dashboard() {
   const location = useLocation();
@@ -22,6 +23,8 @@ export default function Dashboard() {
       </div>
       {/* profile */}
       {tab === "profile" && <DashProfile />}
+
+      {tab==="posts" && <DashPost/>}
     </div>
   );
 }
