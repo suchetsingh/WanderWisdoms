ba9221cc12abe35a07e5ca2d1c77878bc1577d78
suchetsingh <ssingh21be20@gmail.com>
2024-04-01 18:34:11 +0530

    Added Updated Post Functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { verifiyToken } from "../utils/verifiedUser.js";
-import { create, deletepost, getposts } from "../controllers/post.controller.js";
+import { create, deletepost, getposts, updatepost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifiyToken, create);
 router.get("/getposts", getposts);
 router.delete("/deletepost/:postId/:userId", verifiyToken, deletepost);
+router.put("/updatepost/:postId/:userId",verifiyToken,updatepost)
 
 export default router;
