75d3ee058c88d914651a51a487114bc6d72b6370
suchetsingh <ssingh21be20@gmail.com>
2024-04-08 13:59:58 +0530

    Created Get ueer Api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, signout, test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, getUsers, signout, test, updateUser } from "../controllers/user.controller.js";
 import { verifiyToken } from "../utils/verifiedUser.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get("/test", test);
 router.put("/update/:userId",verifiyToken,updateUser);
 router.delete("/delete/:userId",verifiyToken,deleteUser);
 router.post("/signout",signout);
+router.get("/getusers",verifiyToken,getUsers);
 
 export default router;
